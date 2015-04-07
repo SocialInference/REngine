a2322cce0cb76e3b002bf9500cce4f7149730ac9
Beenish <kbeenish@hotmail.com>
2015-04-06 22:39:26 -0600

    Fixup toItemSets

--- to_ItemSets.R
+++ to_ItemSets.R
@@ -1,5 +1,3 @@
-
-
 # To be able to call arules ruleInduction, we need to format data as an itemsets
 # This method formats data in the form of a sparse array with a support column
 # to an itemsets
@@ -7,23 +5,25 @@
 # support is a vector defining the support for each itemset as a data.frame
 toItemSets <- function(setMatrix, sup)
 {
-  itemz <- as(something, "itemMatrix")
+  itemz <- as(setMatrix, "itemMatrix")
   sets <- new("itemsets", items = itemz, quality = sup, tidLists=NULL)
-  return sets
+  return(sets)
 }
 
 # test
-#matrix <- matrix(c(0,1,1,1,
-#                   0,1,1,0,
-#                   0,0,1,1,
-#                   0,1,0,1,
-#                   0,1,0,0,
-#                   0,0,1,0,
-#                   0,0,0,1),
-#                 nrow=4, 
-#                 ncol=7
-#                 )
-#rownames(matrix) <- c("a","b","c","d")
-#sup <- as.data.frame(c(.3,.4,.4,.4,.8,.8,.8))
-#sets <- toItemSets(matrix,sup)
-#inspect(sets)
+matrix <- matrix(c(0,0,0,1,
+                   0,0,1,0,
+                   0,0,1,1),
+                 nrow=3, 
+                 ncol=4
+                 )
+colnames(matrix) <- c("a","b","c","d")
+sup <- as.data.frame(c(.1, .3, .3))
+sets <- toItemSets(matrix,sup)
+inspect(sets)
+ruleInduction(sets)
+
+
+ec  <- eclat(Adult, parameter = list(support = 0.4))
+rec <- ruleInduction(ec)
+inspect(rec[1:5])
