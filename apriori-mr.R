fa804bf34c92635ff28aaa6bb0d9e2efcb90c949
Sean Mikalson <sean.mikalson@gmail.com>
2015-04-06 23:07:13 -0600

    candidate set generation

--- apriori-mr.R
+++ apriori-mr.R
@@ -58,44 +58,128 @@ AdultUCI[["capital-loss"]]   <-ordered(cut(AdultUCI[["capital-loss"]],c(-Inf, 0,
 ### 3- Create dummy variables per factor (required by the Apriori algorithm 
 Adult<-as(AdultUCI,'transactions')
 Adult<-as(Adult,'matrix')
-write.table(Adult[1:500,],row.names=FALSE,col.names=FALSE,sep=',', file='adult.csv')
-to.dfs(Adult[1:500], output='adult.csv',format='csv')
+write.table(Adult[1:1000,],row.names=FALSE,col.names=FALSE,sep=',', file='adult.csv')
+dfs.rmr('adult.csv')
+
+my.out.form<-make.output.format(
+  "csv", 
+  sep = ",")
+to.dfs(Adult[1:1000,], output='adult.csv',format=my.out.form)
 
 my.form<-make.input.format(
   "csv", 
   sep = ",",
   col.names=colnames(Adult))
 
-total<-500
+total<-1000
 min.support<-0.01
 
 map.p1<-function(.,t) {
   d<-dim(t)
-  cols<-colnames(t)
-  size<-length(cols)
+  size<-d[2]
   vals<-matrix(nrow=size,ncol=d[1])
+  keys<-numeric(0)
   for(i in 1:size) {
     vals[i,]<-t[,i]
+    itemset<-numeric(size)
+    itemset[i]<-1
+    keys<-rbind(keys,itemset)
   }
-  keyval(cols,vals)
+  keydf<-as.data.frame(keys)
+  colnames(keydf)<-colnames(t)
+  keyval(keydf,vals)
 }
 
 reduce.p1<-function(k,v) {
   s<-sum(v)
-  support<-s/total
-  if(support>=min.support) {
+  support<-s/1000
+  if(support>=0.01) {
     keyval(k,support)
   }
 }
 
-mr.1<-mapreduce(input="adult.csv", map=map.p1, reduce=reduce.p1, input.format=my.form)
-kvs<-cbind(keys(from.dfs(mr.1)),values(from.dfs(mr.1)))
-kvs<-kvs[order(kvs[,2],kvs[,1]),]
-View(kvs)
+mr.1<-mapreduce(input="adult.csv", map=map.p1,reduce=reduce.p1, input.format=my.form)
+itemsets.1<-keys(from.dfs(mr.1))
+support.1<-values(from.dfs(mr.1))
+View(itemsets.1)
+View(support.1)
 
-freqitems <- apriori(as(Adult[1:500,],'transactions'), parameter=list(support=0.01,maxlen=1,target='frequent itemsets'))
+freqitems <- apriori(as(Adult[1:1000,],'transactions'), parameter=list(support=0.01,maxlen=1,target='frequent itemsets'))
 inspect(freqitems)
 View(data.frame(kvs[,1],kvs[,2],as(freqitems,'data.frame')))
 
+ap_gen<-function(items) {
+  dimitems<-dim(items)
+  numsets<-dimitems[1]
+  numcols<-dimitems[2]
+  candidates<-numeric(0)
+  
+  # 1-itemsets are an exception
+  if(sum(items[1,]) == 1) {
+    for(i in 1:numsets) {
+      for(j in i:numsets) {
+        if(i != j) {
+          prospect<-items[i,] + items[j,]
+          candidates<-rbind(candidates,prospect)
+        }
+      }
+    }
+  }
+  else {
+    for(col in 1:numcols) {
+      #browser()
+      # finding a row to compare and generate union sets off of
+      testrow<-0
+      for(r in 1:numsets) {
+        
+        # looking for leading one row for the current column
+        # This eliminates duplicate candidates from being generated
+        leadingone<-numeric(col)
+        leadingone[col]<-1
+        for(i in 1:col) {
+          if(items[r,i] != leadingone[i]) {
+            testrow<-0
+            break
+          } else {
+            testrow<-r
+          }
+        }
+        if(testrow!=0) break
+      }
+      
+      if(testrow!=0) {
+        # Comparing rows and adding as a way to generate candidates
+        for(r in 1:numsets) {
+          
+          leadingone<-numeric(col)
+          leadingone[col]<-1
+          # Look for a leading one row for current column to properly add similar sets
+          for(i in 1:col) {
+            if(items[r,i] != leadingone[i]) {
+              comparerow<-0
+              break
+            } else {
+              comparerow<-r
+            }
+          }
+          
+          if(comparerow!=0 && comparerow!=testrow) {
+            prospect<-items[r,] + items[testrow,]
+            prospect<-ifelse(prospect==2,1,prospect)
+            candidates<-rbind(candidates,prospect)
+          }
+        }
+      }
+    }
+  }
+ 
+  candidates
+}
+
+d<-diag(5)
+d[2,1]<-1
+d[5,3]<-1
+x<-ap_gen(diag(5))
+y<-ap_gen(x)
 
 
